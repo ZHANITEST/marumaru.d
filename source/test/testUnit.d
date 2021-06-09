4752eef8cc8bbba639fe2f95c493abbd798119d3
ZHANITEST <zhanitest@hotmail.com>
2021-06-10 01:47:31 +0900

    유닛테스트 추가

--- source/test/testUnit.d
+++ source/test/testUnit.d
@@ -2,6 +2,7 @@ module testUnit;
 
 import com.zhanitest.marumaru.unit;
 import com.zhanitest.marumaru.utils;
+import com.zhanitest.marumaru.common;
 import std.conv:to;
 
 unittest {
@@ -9,6 +10,12 @@ unittest {
     // Comic 유닛테스트
     //==========================================================
     Comic comic = new Comic("22739");
+    
+    // URL 조립체크
+    // 기대값: https://marumaru.cloud/bbs/cmoic/22739
+    assert(comic.url == CommonData.HOST~"/"~CommonData.BOARD~"/22739",
+        comic.url);
+
     //comic.fakeLoad(); /* 로컬 테스트 */
     comic.load(); /* 리얼 테스트 */
     
@@ -37,12 +44,13 @@ unittest {
     //page.load(); /* 리얼 테스트 */
     
     // URL 조립체크
-    assert("https://marumaru.cloud/bbs/cmoic/22739/35937"==page.url,
+    // 기대값: https://marumaru.cloud/bbs/cmoic/22739/35937
+    assert(CommonData.HOST~"/"~CommonData.BOARD~"/22739/35937"==page.url,
         page.url);
 
     // getTitle 타이틀 얻기
-    assert("잘 자, 푼푼 1-1권"==page.title,
-        page.title);
+    assert("잘 자, 푼푼 1-1권"==page.getTitle(),
+        page.getTitle());
 
     assert(page.getImageUrls().length == 118
         , page.getImageUrls().length.to!string());
